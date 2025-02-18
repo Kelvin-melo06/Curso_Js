2a09561d1ee2c554e0df8fba261fc1d98b3c1c2d
Kelvin melo <kmelo7882@gmail.com>
2025-02-18 14:36:13 -0300

    comited

---
+++ desafio1.js
@@ -0,0 +1,9 @@
+let preco_Combustivel = 4.13;
+let combustivel_Por_Km = 8;
+let distancia_Viagem = 1580;
+
+let combustivel_Gasto = distancia_Viagem / combustivel_Por_Km;
+let valor_Viagem = combustivel_Gasto * preco_Combustivel;
+console.log(`O valor gasto na viagem foi de ${valor_Viagem.toFixed(2)} Reais`);
+
+// .toFixed() é uma função que arruma as casas decimais de um numero e em consequencia transforma esse númmero em um string
