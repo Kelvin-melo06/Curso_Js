9596615069b6495167b8ebf0afba6821c31b3971
Kelvin melo <kmelo7882@gmail.com>
2025-02-26 13:18:16 -0300

    comited

--- juros_com_funcoes.js
+++ juros_com_funcoes.js
@@ -1,8 +1,20 @@
+/*
+
+ Condição pagamento:
+ Á vista Débito, recebe 10% de desconto;
+ Á vista no Dinheiro ou Pix, recebbe 15% de desconto;
+ Em duas vezes, preço norma de etiqueta;
+ Acima de duas vezes, preço normal de etiqueta mais juros de 10%;
+
+*/
+
+
+
 function tipo_pagamento(pagamento, preco_etiqueta){
     if(pagamento === 'Débito'){
         let desconto_debito = preco_etiqueta - (preco_etiqueta * 0.10)
         return (desconto_debito.toFixed(3));
-    } else if(pagamento === "Dinheiro" || tipo_pagamento === "Pix"){
+    } else if(pagamento === "Dinheiro" || pagamento === "Pix"){
        let desconto_dinheiro_pix = preco_etiqueta - (preco_etiqueta * 0.15)
         return (desconto_dinheiro_pix.toFixed(3));
     }  else if(pagamento === 'Duas vezes'){
@@ -13,16 +25,9 @@ function tipo_pagamento(pagamento, preco_etiqueta){
     }
 }
 
-function main(){
+(function(){
     let preco_etiqueta = 6.499;
-    let tipo_De_Pagamento = tipo_pagamento('Débito', preco_etiqueta);
-    console.log(`${tipo_De_Pagamento}`);
-}
-
-main();
-
-
-
-
-
-
+    let pagamento_tipo = 'Dinheiro';
+    let condicao_De_Pagamento = tipo_pagamento(pagamento_tipo, preco_etiqueta);
+    console.log(`No ${pagamento_tipo} o valor final fica ${condicao_De_Pagamento} Reais`);
+}())
