a6423b20379256fb0e2de53829e93865233ee99f
Kelvin melo <kmelo7882@gmail.com>
2025-04-08 16:37:29 -0300

    comited

--- importação/main.js
+++ importação/main.js
@@ -1,19 +1,14 @@
 const {gets, print} = require('./funcoesauxiliares') // o REQUIRE serve para importação do que está sendo exportado
 
-const numerosSorteados = [];
-
-for(i = 0; i < 5; i++){
-    let numeroSorteado = gets();
-    numerosSorteados.push(numeroSorteado);
-}
-
+let quantidadeAlunos = gets();
 let maiorNumero = 0;
 
-for(i = 0; i < numerosSorteados.length; i++){
-    let numeroSorteado = numerosSorteados[i];
+for(i = 0; i < quantidadeAlunos; i++){
+    let numeroSorteado = gets();
     if(numeroSorteado > maiorNumero){
         maiorNumero = numeroSorteado;
     }
 }
+
 
 print(maiorNumero);
