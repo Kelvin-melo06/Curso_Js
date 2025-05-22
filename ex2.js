0e7be81a874f54add2b6e786f72a61b93e0cac79
Kelvin Melo <kmelo7882@gmail.com>
2025-05-22 16:54:33 -0300

    comentarios

--- ex2.js
+++ ex2.js
@@ -1,6 +1,18 @@
+
 let iniciaisNome = (str) =>{
     return str.split(" ").map((n) => n[0].toUpperCase()).join(".");
 }
 
 
 console.log(iniciaisNome("Lucas Silva"));
+
+
+/*
+function fn(x){
+    let y = x.toLowerCase();
+    let z = y.trim();
+    return z[0];
+}
+
+console.log(fn("Lucas Silva"));
+*/
