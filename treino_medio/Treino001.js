d3ebc8e27f06a3951466910869a49852fa2e6ecc
Kelvin Melo <kmelo7882@gmail.com>
2025-08-18 13:20:23 -0300

    adicionando metodoreduce

--- treino_medio/Treino001.js
+++ treino_medio/Treino001.js
@@ -1,22 +1,20 @@
 let aluno = {
-    nome: "Leonardo",
-    notas: [10, 9, 8]
-}
+  nome: "Leonardo",
+  notas: [10, 9, 8],
+};
 
 let notaFinal = (aluno) => {
-   let soma = 0;
-    for (let i = 0; i < aluno.notas.length; i++) {
-         soma += aluno.notas[i];
-    }
-    let media = soma / aluno.notas.length;
+  let soma = aluno.notas.reduce((acumulador, nota) => {
+    return acumulador + nota;
+  }, 0);
+  let media = soma / aluno.notas.length;
 
-    if(media >= 7) {
-        console.log("Aprovado com média: " + media);
-    } else{
-        console.log("Reprovado com média: " + media);
-    }
-    return media;
-}
+  if (media >= 7) {
+    console.log("Aprovado com média: " + media);
+  } else {
+    console.log("Reprovado com média: " + media);
+  }
+  return media;
+};
 
 console.log(notaFinal(aluno));
-
