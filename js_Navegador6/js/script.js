76b76c160290fe551466e1033901d4e6bd0525a3
Kelvin Melo <kmelo7882@gmail.com>
2025-09-02 17:41:19 -0300

    metodos Dom

---
+++ js_Navegador6/js/script.js
@@ -0,0 +1,32 @@
+const lista = document.querySelector("ul");
+
+// ------------------------REPLACECHILD-------------------------------------
+
+const novoPrimeiroItem = document.createElement('li');
+novoPrimeiroItem.textContent = 'Novo Primeiro Item';
+
+const anteriorPrimeiroItem = document.getElementById('firstChild');
+
+lista.replaceChild(novoPrimeiroItem, anteriorPrimeiroItem);
+
+// ------------------------------------------------------------------------
+
+//------------------------ APPENDCHILD --------------------------------------
+
+const novoItem = document.createElement('li');
+novoItem.textContent = 'Novo item adicionado';
+
+lista.appendChild(novoItem);
+
+// --------------------------------------------------------------------------
+
+// ----------------------- INSERTBEFORE ------------------------------------ 
+
+const novoItemInseridoAntesDoPrimeiro = document.createElement('li');
+novoItemInseridoAntesDoPrimeiro.textContent = 'novoItemInseridoAntesDoPrimeiro';
+
+
+lista.insertBefore(novoItemInseridoAntesDoPrimeiro, novoPrimeiroItem);
+
+// --------------------------------------------------------------------------
+
